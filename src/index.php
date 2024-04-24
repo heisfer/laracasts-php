4e15d30286790cc1720a34646745fca0e3acda0c
heisfer <heisfer@aol.com>
2024-04-25 00:54:51 +0300

    Episode 17

--- src/index.php
+++ src/index.php
@@ -1,3 +1,19 @@
 <?php
 require 'functions.php';
-require 'router.php';
+//require 'router.php';
+
+// connect to our MySQL database
+
+$dsn = "mysql:host=localhost;port=3306;dbname=app;charset=utf8mb4";
+$username = "app";
+$password = "app";
+$pdo = new PDO($dsn, $username, $password);
+
+$statement = $pdo->prepare("SELECT * FROM posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+  echo "<li>" . $post['title'] . "</li>";
+}
