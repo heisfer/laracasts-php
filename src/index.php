561a96e5df29c89ceeb1889b96e3703c3aa08b81
heisfer <heisfer@aol.com>
2024-04-24 21:48:20 +0300

    Episode 12

--- src/index.php
+++ src/index.php
@@ -1,14 +1,2 @@
 <?php
-
-$business = [
-  'name' => 'Laracasts',
-  'cost' => 15,
-  'categories' => ['Testing', 'PHP', 'JavaScript']
-];
-
-function register($user)
-{
-}
-
-
 include 'index.view.php';
