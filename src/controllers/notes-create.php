f520749d066f6c04b41b915a1c149ae47ee4488d
heisfer <heisfer@aol.com>
2024-04-25 14:05:18 +0300

    Episode 27

--- src/controllers/notes-create.php
+++ src/controllers/notes-create.php
@@ -5,10 +5,21 @@ $config = require 'config.php';
 $db = new Database($config['database']);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $body = $_POST['body'];
+  $errors = [];
+  if (strlen($body) === 0) {
+    $errors['body'] = 'A body is required';
+  }
 
-  $db->query('INSERT INTO notes(body, user_id) VALUES (:body, :user_id)', [
-    'body' => $_POST['body'],
-    'user_id' => 2
-  ]);
+  if (strlen($body) >= 1000) {
+    $errors['body'] = 'The Body can not be more than 1,000 characters.';
+  }
+
+  if (empty($errors)) {
+    $db->query('INSERT INTO notes(body, user_id) VALUES (:body, :user_id)', [
+      'body' => $_POST['body'],
+      'user_id' => 2
+    ]);
+  }
 }
 require 'views/notes-create.view.php';
