39f6904e468f2539a4c4999bdb645a19de8c8abe
heisfer <heisfer@aol.com>
2024-04-27 23:14:34 +0300

    Episode 38

---
+++ controllers/registration/create.php
@@ -0,0 +1,5 @@
+<?php
+
+view('registration/create.view.php', [
+  'heading' => 'Create an account'
+]);
--- routes.php
+++ routes.php
@@ -29,3 +29,6 @@ $router->get('/note/edit', 'controllers/notes/edit.php');
 
 $router->get('/contact', 'controllers/contact.php');
 $router->get('/mission', 'controllers/mission.php');
+
+$router->get('/register', 'controllers/registration/create.php');
+$router->post('/register', 'controllers/registration/store.php');
